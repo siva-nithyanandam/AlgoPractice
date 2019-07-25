3d47da5d2d5fe557edb2b0a50212e1da92dc2a9c
Sivaprakash Nithyanandam <trysivaprakash@gmail.com>
2019-07-25 00:20:16 -0400

    Minimal tree

--- src/TreesAndGraphs/MinimalTree.java
+++ src/TreesAndGraphs/MinimalTree.java
@@ -6,4 +6,38 @@ package TreesAndGraphs;
  */
 public class MinimalTree {
 
+    static class Node {
+        private int data;
+        private Node left;
+        private Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    private static Node createBST(int[] arr, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int n = (start + end)/2;
+        Node root = new Node(arr[n]);
+        root.left = createBST(arr, start, n - 1);
+        root.right = createBST(arr, n + 1, end);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        Node result = createBST(arr, 0, arr.length-1);
+        preOrderTraversal(result);
+    }
+
+    private static void preOrderTraversal(Node node) {
+        if (node != null) {
+            System.out.print(node.data + ",");
+            preOrderTraversal(node.left);
+            preOrderTraversal(node.right);
+        }
+    }
 }
