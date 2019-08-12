8520086ce130a56ba4677bd1b6109ef8aa3a921c
Sivaprakash Nithyanandam <trysivaprakash@gmail.com>
2019-08-12 00:31:55 -0400

    More merge problems

---
+++ src/linkedlists/MergeKSortedLists.java
@@ -0,0 +1,26 @@
+package linkedlists;
+
+/**
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+ *
+ * Example:
+ *
+ * Input:
+ * [
+ *   1->4->5,
+ *   1->3->4,
+ *   2->6
+ * ]
+ * Output: 1->1->2->3->4->4->5->6
+ */
+public class MergeKSortedLists {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+ }
+
+public ListNode mergeKLists(ListNode[] lists) {
+    return null;
+}
+}
