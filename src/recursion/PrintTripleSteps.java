607c7d4fb06e8ed06fe78aee8cc91dd16caddf37
Sivaprakash Nithyanandam <trysivaprakash@gmail.com>
2019-08-23 09:00:33 -0400

    Print triple steps

--- src/recursion/PrintTripleSteps.java
+++ src/recursion/PrintTripleSteps.java
@@ -1,11 +1,42 @@
 package recursion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Print ways to reach the n’th stair
+ * Print ways to reach the n’th stair taking 1, 2 or 3 hops possibilities.
  */
 
 /**
- * https://stackoverflow.com/questions/32818911/print-ways-to-reach-the-n-th-stair
+ * Recursion problem.
+ * Increment the steps of possibilities to the current step and check it reaches n'th stair.
  */
 public class PrintTripleSteps {
+    public static void main(String[] args) {
+        printWaysToReach(6);
+    }
+
+    private static void printWaysToReach(int steps) {
+        List<Integer> list = new ArrayList<Integer>();
+        printWaysToReach(steps, 0, list);
+    }
+
+    private static void printWaysToReach(int steps, int curr, List<Integer> list) {
+        if (curr > steps) {
+            return;
+        }
+        list.add(curr);
+        if (curr == steps) {
+            for (int i = 1; i < list.size(); i++) {
+                System.out.print(list.get(i) + " ");
+            }
+            System.out.println("");
+            list.remove(list.size() - 1);
+            return;
+        }
+        printWaysToReach(steps, curr + 1, list);
+        printWaysToReach(steps, curr + 2, list);
+        printWaysToReach(steps, curr + 3, list);
+        list.remove(list.size() - 1);
+    }
 }
