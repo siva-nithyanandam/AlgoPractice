421b52e685cc7f258929a0bce63b99ec1f9d5e19
Sivaprakash Nithyanandam <trysivaprakash@gmail.com>
2019-08-25 01:12:47 -0400

    Adding two sum

--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -20,22 +20,36 @@ import java.util.HashMap;
  */
 public class TwoSum {
   public static void main(String[] args) {
-    int[] arr = {2, 7, 11, 15, 0};
-    int target = 11;
+    int[] arr;
+    int target;
+    int[] res;
 
-    int[] res = findSumIndices(arr, target);
+    arr = new int[]{0, 4, 3, 0};
+    target = 0;
+    res = findSumIndices(arr, target);
     System.out.println(res[0] + " " + res[1]);
+
+    arr = new int[]{230,863,916,585,981,404,316,785,88,12,70,435,384,778,
+            887,755,740,337,86,92,325,422,815,650,920,125,277,336,221,847,168,23,677,61,
+            400,136,874,363,394,199,863,997,794,587,124,321,212,957,764,173,314,422,927,
+            783,930,282,306,506,44,926,691,568,68,730,933,737,531,180,414,751,28,546,60,
+            371,493,370,527,387,43,541,13,457,328,227,652,365,430,803,59,858,538,427,583,368,
+            375,173,809,896,370,789};
+    target = 542;
+    res = findSumIndices(arr, target);
+    System.out.println(res[0] + " " + res[1]);
+
   }
 
-  private static int[] findSumIndices(int[] arr, int target) {
-    Map<Integer, Integer> checker = new HashMap<Integer, Integer>();
-    for(int i = 0; i < arr.length; i++) {
-      int delta = target - arr[i];
-      if (checker.containsKey(delta)) {
-        return new int[] {checker.get(delta), i};
+  private static int[] findSumIndices(int[] nums, int target) {
+    int[] holder = new int[2048];
+    for (int i = 0; i < nums.length; i++) {
+      int delta = (target - nums[i]) & 2047;
+      if (holder[delta] != 0) {
+        return new int[]{holder[delta] - 1, i};
       }
-      checker.put(arr[i], i);
+      holder[nums[i] & 2047] = i + 1;
     }
-    return new int[] {-1, -1};
+    return new int[]{-1, -1};
   }
 }
