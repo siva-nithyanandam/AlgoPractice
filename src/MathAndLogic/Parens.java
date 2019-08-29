469b28d9b2e9bfb634020790f7f823a3762a5de4
Sivaprakash Nithyanandam <trysivaprakash@gmail.com>
2019-08-29 01:16:08 -0400

    Parens

--- src/MathAndLogic/Parens.java
+++ src/MathAndLogic/Parens.java
@@ -7,4 +7,29 @@ package MathAndLogic;
  * Input: 3 Output: ((())), (()()), (())(), ()(()), ()()()
  */
 public class Parens {
+    public static void main(String[] args) {
+        printParenthesis(3);
+    }
+
+    private static void printParenthesis(int n) {
+        char[] arr = new char[2*n];
+        printParenthesis(n, arr, 0, 0, 0);
+    }
+
+    private static void printParenthesis(int n, char[] arr, int index, int open, int close) {
+        if (index == arr.length) {
+            for (char c : arr) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+        if (open < n) {
+            arr[index] = '(';
+            printParenthesis(n, arr, index + 1, open+1, close);
+        }
+        if (close < open) {
+            arr[index] = ')';
+            printParenthesis(n, arr, index + 1, open, close+1);
+        }
+    }
 }
