0e844f1dc053f588e962e0f01acfef56a1a100ef
Sivaprakash <SNITHY11@ford.com>
2019-09-04 17:49:49 -0400

    Boolean evaluation

---
+++ src/MathAndLogic/BooleanEvaluation.java
@@ -0,0 +1,18 @@
+package MathAndLogic;
+
+/**
+ * Given a boolean expression consisting of the symbols e (false), 1 (true), &
+ * (AND), I (OR), and" (XOR), and a desired boolean result value result, implement a function to
+ * count the number of ways of parenthesizing the expression such that it evaluates to result.
+ * EXAMPLE
+ * countEval("1^0|0|1", false) -> 2
+ * countEval("0&0&0&1^1|0", true) -> 10
+ */
+
+/**
+ * https://prismoskills.appspot.com/lessons/Dynamic_Programming/Chapter_27_-_Max_ways_for_boolean_expression_to_be_true.jsp
+ * https://www.youtube.com/watch?v=oyj9tRZhmis
+ */
+public class BooleanEvaluation {
+
+}
