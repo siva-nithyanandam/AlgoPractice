75746a78b0c53c48d3e34cff74e44d3a34579c77
Sivaprakash Nithyanandam <trysivaprakash@gmail.com>
2019-09-24 03:13:47 -0400

    House robber II

---
+++ src/TreesAndGraphs/BSTIterator.java
@@ -0,0 +1,88 @@
+package TreesAndGraphs;
+
+import java.util.Stack;
+
+/**
+ * Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
+ * Calling next() will return the next smallest number in the BST.
+ *
+ *
+ * Example:
+ *     7
+ *    / \
+ *   3   15
+ *       / \
+ *      9   20
+ * BSTIterator iterator = new BSTIterator(root);
+ * iterator.next();    // return 3
+ * iterator.next();    // return 7
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 9
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 15
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 20
+ * iterator.hasNext(); // return false
+ *
+ * Note:
+ * next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+ * You may assume that next() call will always be valid, that is,
+ * there will be at least a next smallest number in the BST when next() is called.
+ */
+public class BSTIterator {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(7);
+        node.left = new TreeNode(3);
+        node.right = new TreeNode(15);
+        node.right.left = new TreeNode(9);
+        node.right.right = new TreeNode(20);
+        BSTIterator iterator = new BSTIterator(node);
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+        System.out.println(iterator.hasNext());
+        System.out.println(iterator.next());
+        System.out.println(iterator.hasNext());
+        System.out.println(iterator.next());
+        System.out.println(iterator.hasNext());
+        System.out.println(iterator.next());
+        System.out.println(iterator.hasNext());
+    }
+
+    TreeNode root;
+    Stack<TreeNode> stack = new Stack<>();
+    public BSTIterator(TreeNode root) {
+        this.root = root;
+        TreeNode curr = root;
+        while (curr != null) {
+            stack.push(curr);
+            curr = curr.left;
+        }
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode res = stack.pop();
+        TreeNode curr = res.right;
+        while (curr != null) {
+            stack.push(curr);
+            curr = curr.left;
+        }
+        return res.val;
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return stack.size() > 0;
+    }
+}
