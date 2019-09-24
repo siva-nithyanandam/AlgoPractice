47e9ba75ec0387d1e75c598d483f68fb455a5e48
Sivaprakash <SNITHY11@ford.com>
2019-09-24 19:34:14 -0400

    Factor combinations

--- src/MathAndLogic/FactorCombinations.java
+++ src/MathAndLogic/FactorCombinations.java
@@ -1,5 +1,9 @@
 package MathAndLogic;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * Numbers can be regarded as product of its factors. For example,
  * 8 = 2 x 2 x 2;
@@ -9,6 +13,57 @@ package MathAndLogic;
  * You may assume that n is always positive.
  * Factors should be greater than 1 and less than n.
  */
-public class FactorCombinations {
 
+/**
+ * Get factors upto the given square root, and recursive call for quotient.
+ */
+public class FactorCombinations {
+  public static void main(String[] args) {
+    FactorCombinations o = new FactorCombinations();
+    List<List<Integer>> res = new ArrayList<>();
+    o.getFactors(8, res, new Stack<>());
+    printRes(res);
+
+    res.clear();
+    o.getFactors(10, res, new Stack<>());
+    printRes(res);
+
+    res.clear();
+    o.getFactors(100, res, new Stack<>());
+    printRes(res);
+
+    res.clear();
+    o.getFactors(72, res, new Stack<>());
+    printRes(res);
+  }
+
+  private static void printRes(List<List<Integer>> res) {
+    for (List<Integer> list : res) {
+      for (Integer i : list) {
+        System.out.print(i + ",");
+      }
+      System.out.println();
+    }
+    System.out.println();
+  }
+
+  private void getFactors(int num, List<List<Integer>> res, Stack<Integer> subRes) {
+    if (num == 1) {
+      return;
+    }
+    List<Integer> subSubRes;
+    for (int i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i == 0) {
+        subSubRes = new ArrayList<>();
+        subSubRes.addAll(subRes);
+        subSubRes.add(i);
+        subSubRes.add(num/i);
+        res.add(subSubRes);
+
+        subRes.add(i);
+        getFactors(num/i, res, subRes);
+        subRes.pop();
+      }
+    }
+  }
 }
