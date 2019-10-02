ba6c084985ed7a88e4b7e14eca121b018c97b9bb
Sivaprakash Nithyanandam <trysivaprakash@gmail.com>
2019-10-02 00:26:21 -0400

    Minimum moves to reach target

--- src/arrays/MinimumMovestoReachTargetwithRotations.java
+++ src/arrays/MinimumMovestoReachTargetwithRotations.java
@@ -46,78 +46,115 @@ import java.util.Queue;
 public class MinimumMovestoReachTargetwithRotations {
 
   private static final int HORIZONTAL = 0;
-  private static final int VERTICAL = 0;
+  private static final int VERTICAL = 1;
   public static void main(String[] args) {
     MinimumMovestoReachTargetwithRotations o = new MinimumMovestoReachTargetwithRotations();
-    int[][] grid = new int[][]{{0,0,0,0,0,1},
+    int[][] grid1 = new int[][]
+        {{0,0,0,0,0,1},
         {1,1,0,0,1,0},
         {0,0,0,0,1,1},
         {0,0,1,0,1,0},
         {0,1,1,0,0,0},
         {0,1,1,0,0,0}};
-    System.out.println(o.minimumMoves(grid));
+
+    int[][] grid = new int[][]{{0,0,0,0},
+            {0,0,0,0},
+            {0,0,0,0},
+            {0,0,0,0}};
+    System.out.println(o.minimumMoves(grid1));
   }
 
-  public int minimumMoves(int[][] g) {
-    if (g.length == 0) {
+  public int minimumMoves(int[][] grid) {
+    if (grid.length == 0) {
       return -1;
     }
-    int n = g.length;
-    int m = g[0].length;
+    int n = grid.length;
+    int m = grid[0].length;
     Queue<int[]> q = new ArrayDeque<>();
     q.add(new int[]{0,0,HORIZONTAL});
-    int[][] dist = new int[n][m];
-    for(int i = 0;i < n;i++){
-      for(int j = 0;j < m;j++){
-        dist[i][j] = 99999;
+    int[][][] dist = new int[2][n][m];
+    for(int i = 0;i < 2;i++){
+      for(int j = 0;j < n;j++){
+        for(int k = 0;k < m;k++){
+          dist[i][j][k] = 99999;
+        }
       }
     }
-    dist[0][0] = 0;
-    dist[0][1] = 0;
+    dist[HORIZONTAL][0][0] = 0;
 
     while(!q.isEmpty()) {
       int[] loc = q.poll();
       int r = loc[0];
       int c = loc[1];
       int pos = loc[2];
-      int r2 = pos == HORIZONTAL ? r : r+1;
-      int c2 = pos == HORIZONTAL ? c+1 : c;
+      int ur = pos == HORIZONTAL ? r : r+1;
+      int uc = pos == HORIZONTAL ? c+1 : c;
+
+      if(r == n-1 && c == m-2 && pos == HORIZONTAL){
+        return dist[pos][r][c];
+      }
 
       //Slide right - Either Horizontal position or Vertical position
-      if ((c + 1 < m) && (c2 + 1 < m) && g[r][c+1] == 0 && g[r2][c2+1] == 0) {
-        if (dist[r][c]+1 < dist[r][c+1]) {
-          dist[r][c+1] = dist[r][c]+1;
+      if ((uc + 1 < m) && grid[r][c+1] == 0 && grid[ur][uc+1] == 0) {
+        if (dist[pos][r][c+1] > dist[pos][r][c] + 1) {
+          dist[pos][r][c+1] = dist[pos][r][c]+1;
           q.add(new int[]{r,c+1,pos});
         }
       }
 
       //Slide down - Either Horizontal position or Vertical position
-      if ((r + 1 < n) && (r2 + 1 < n) && g[r+1][c] == 0 && g[r2+1][c2] == 0) {
-        if (dist[r][c]+1 < dist[r+1][c]) {
-          dist[r+1][c] = dist[r][c]+1;
+      if ((ur + 1 < n) && grid[r+1][c] == 0 && grid[ur+1][uc] == 0) {
+        if (dist[pos][r+1][c] > dist[pos][r][c] + 1) {
+          dist[pos][r+1][c] = dist[pos][r][c]+1;
           q.add(new int[]{r+1,c,pos});
         }
       }
 
-      //Rotate clockwise if Horizontal
-      if (pos == HORIZONTAL) {
-        if ((r+1 < n) && g[r+1][c] == 0 && g[r2+1][c2+1] == 0) {
-          dist[r][c] = dist[r][c]+1;
-          q.add(new int[]{r,c,VERTICAL});
-        }
-      }
-
-      //Rotate clockwise if Horizontal
-      if (pos == VERTICAL) {
-        if ((c+1 < m) && g[r][c+1] == 0 && g[r2+1][c2+1] == 0) {
-          dist[r][c] = dist[r][c]+1;
-          q.add(new int[]{r,c,HORIZONTAL});
+      int xr = r + (pos == HORIZONTAL ? 1 : 0);
+      int xc = c + (pos == HORIZONTAL ? 0 : 1);
+      if (xr < n && xc < m && grid[xr][xc] == 0 && grid[r+1][c+1] == 0) {
+        if (dist[pos ^ 1][r][c] > dist[pos][r][c] + 1) {
+          dist[pos ^ 1][r][c] = dist[pos][r][c] + 1;
+          q.add(new int[]{r, c, pos^1});
         }
       }
     }
-    return dist[n-1][m-2];
+    return -1;
   }
 
+  public int minimumMoves2(int[][] grid) {
+    int[][] vV = new int[grid.length][grid[0].length];
+    int[][] vH = new int[grid.length][grid[0].length];
+    return check(grid, 0, 0, false, vV, vH, 0);
+  }
+
+  private int check(int[][] grid, int row, int col, boolean up, int[][] vV, int[][] vH, int steps) {
+    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length)
+      return -1;
+    else if (up && (row + 1 >= grid.length || grid[row][col] == 1 || grid[row + 1][col] == 1 || (vH[row][col] != 0 && steps >= vH[row][col])))
+      return -1;
+    else if (!up && (col + 1 >= grid[0].length || grid[row][col] == 1 || grid[row][col + 1] == 1 || (vV[row][col] != 0 && steps >= vV[row][col])))
+      return -1;
+
+    if (row == grid.length - 1 && col == grid[0].length - 2 && up == false)
+      return steps;
+
+    if (up) vH[row][col] = steps;
+    else vV[row][col] = steps;
+    // System.out.println(row + " " + col + " " + steps + " " + up);
+
+    int res = Integer.MAX_VALUE;
+    int rt = check(grid, row, col + 1, up, vV, vH, steps + 1);
+    if (rt != -1 && rt < res) res = rt;
+    rt = check(grid, row + 1, col, up, vV, vH, steps + 1);
+    if (rt != -1 && rt < res) res = rt;
+    if (row + 1 < grid.length && col + 1 < grid[0].length && grid[row][col] != 1 && grid[row + 1][col] != 1 && grid[row][col + 1] != 1 && grid[row + 1][col + 1] != 1) {
+      rt = check(grid, row, col, !up, vV, vH, steps + 1);
+      if (rt != -1 && rt < res) res = rt;
+    }
+
+    return res == Integer.MAX_VALUE ? -1 : res;
+  }
 
   public int minimumMoves1(int[][] g) {
     int n = g.length, m = g[0].length;
