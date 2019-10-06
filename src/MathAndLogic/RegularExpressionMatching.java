0f304d11f862fba5853ee581a790d21446316a7b
Sivaprakash Nithyanandam <trysivaprakash@gmail.com>
2019-10-06 01:23:07 -0400

    reg ex matching

--- src/MathAndLogic/RegularExpressionMatching.java
+++ src/MathAndLogic/RegularExpressionMatching.java
@@ -38,13 +38,77 @@ package MathAndLogic;
  * p = "mis*is*p*."
  * Output: false
  */
+
+/**
+ * Using dynamic programming - Bottom Up approach
+ */
 public class RegularExpressionMatching {
 
   public static void main(String[] args) {
-
+    RegularExpressionMatching o = new RegularExpressionMatching();
+    System.out.println(o.isMatch("xaabyc", "xa*b.c"));//true
+    System.out.println(o.isMatch("aa", "a"));//false
+    System.out.println(o.isMatch("aa", "a*"));//true
+    System.out.println(o.isMatch("ab", ".*"));//true
+    System.out.println(o.isMatch("aab", "c*a*b"));//true
+    System.out.println(o.isMatch("mississippi", "mis*is*p*."));//false
+    System.out.println(o.isMatch("mississippi", "mis*is*ip*."));//true
   }
 
   public boolean isMatch(String s, String p) {
-    return false;
+    boolean[][] arr = new boolean[s.length()+1][p.length()+1];
+    arr[0][0] = true;
+    for (int i = 1; i <= p.length(); i++) {
+      if (p.charAt(i-1) == '*') {
+        arr[0][i] = arr[0][i-2];
+      }
+    }
+    for (int i = 1; i <= s.length(); i++) {
+      for (int j = 1; j <= p.length(); j++) {
+        if (s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '.') {
+          arr[i][j] = arr[i-1][j-1];
+        } else if (p.charAt(j-1) == '*') {
+          if (arr[i][j-2]) {
+            arr[i][j] = arr[i][j-2];
+          } else if (s.charAt(i-1) == p.charAt(j-2) || p.charAt(j-2) == '.') {
+            arr[i][j] = arr[i-1][j];
+          }
+        } else {
+          arr[i][j] = false;
+        }
+      }
+    }
+    return arr[s.length()][p.length()];
+  }
+
+  public boolean isMatch_faster(String s, String p) {
+    char[] sChars = s.toCharArray();
+    char[] pChars = p.toCharArray();
+    Boolean [] [] result = new Boolean [s.length() + 1][p.length() + 1];
+    return isMatch(sChars, 0, pChars, 0, result);
+  }
+
+  private boolean isMatch(char [] sChars, int sIndex, char [] pChars, int pIndex, Boolean [] [] result) {
+    if (result[sIndex][pIndex] != null) {
+      return result[sIndex][pIndex];
+    }
+    boolean ret;
+    if (pIndex >= pChars.length) {
+      // if pattern is empty, then it would only match an empty string
+      ret = sIndex == sChars.length;
+    } else {
+
+      boolean matchFirstChar = sIndex < sChars.length
+              && (sChars[sIndex] == pChars[pIndex] || pChars[pIndex] == '.');
+
+      if (pIndex < pChars.length - 1 && pChars[pIndex + 1] == '*') {
+        ret = isMatch(sChars, sIndex, pChars, pIndex + 2, result)
+                || matchFirstChar && isMatch(sChars, sIndex + 1, pChars, pIndex, result);
+      } else {
+        ret = matchFirstChar && isMatch(sChars, sIndex + 1, pChars, pIndex + 1, result);
+      }
+    }
+    result[sIndex][pIndex] = ret;
+    return ret;
   }
 }
