cb10f78d5a4051ccb9fd79d3f0ba7e87383e241d
Sivaprakash <SNITHY11@ford.com>
2019-11-05 17:31:53 -0500

    More progress

--- src/arrays/FourSum.java
+++ src/arrays/FourSum.java
@@ -23,10 +23,19 @@ public class FourSum {
   public static void main(String[] args) {
     FourSum o = new FourSum();
     List<List<Integer>> fourSum;
-    fourSum = o.fourSum(new int[]{-2, -1, 0, 0, 1, 2}, 0);
+    fourSum = o.fourSum_faster(new int[]{-2, -1, 0, 0, 1, 2}, 0);
     printList(fourSum);
 
     fourSum = o.fourSum(new int[]{1,0,-1,0,-2,2}, 0);
+    printList(fourSum);
+
+    fourSum = o.fourSum(new int[]{-3,-2,-1,0,0,1,2,3}, 0);
+    printList(fourSum);
+
+    fourSum = o.fourSum(new int[]{-1,0,1,2,-1,-4}, -1);
+    printList(fourSum);
+
+    fourSum = o.fourSum(new int[]{-1,0,-5,-2,-2,-4,0,1,-2}, -9);
     printList(fourSum);
   }
 
@@ -40,7 +49,151 @@ public class FourSum {
     System.out.println();
   }
 
+  public List<List<Integer>> fourSum_faster(int[] nums, int target) {
+    List<List<Integer>> res = new ArrayList<>();
+    if(nums == null || nums.length < 4) {
+      return res;
+    }
+    Arrays.sort(nums);
+    int max = nums[nums.length - 1];
+    if(4 * nums[0] > target || 4 * max < target) {
+      return res;
+    }
+
+    int z;
+    for(int i = 0; i < nums.length; i++) {
+      z = nums[i];
+      if(i > 0 && z == nums[i - 1])
+        continue;
+      if(z + 3 * max < target)
+        continue;
+      if(4 * z > target)
+        break;
+      if(4 * z == target) {
+        if(i + 3 < nums.length && nums[i + 3]  == z) {
+          res.add(Arrays.asList(z, z, z, z));
+        }
+        break;
+      }
+      threeSumForFourSum(nums, target - z, i + 1, nums.length - 1, res, z);
+    }
+    return res;
+  }
+
+  private void threeSumForFourSum(int[] nums, int target, int low, int high, List<List<Integer>> res, int z1) {
+    if(low + 1 >= high)
+      return;
+    int max = nums[high];
+    if(3* nums[low] > target || 3 * max < target)
+      return;
+
+    int i, z;
+    for (i = low; i < high - 1; i++) {
+      z = nums[i];
+      if (i > low && z == nums[i - 1])
+        continue;
+      if(z + 2 * max < target)
+        continue;
+      if(3 * z > target)
+        break;
+      if(3 * z == target) {
+        if(i + 1 < high && nums[i + 2] == z)
+          res.add(Arrays.asList(z1, z, z, z));
+        break;
+      }
+      twoSumForFourSum(nums, target- z, i + 1, high, res, z1, z);
+    }
+  }
+
+  private void twoSumForFourSum(int[] nums, int target, int low, int high, List<List<Integer>> res, int z1, int z2) {
+    if(low >= high)
+      return;
+    if(2 * nums[low] > target || 2 * nums[high] < target)
+      return;
+
+    int i = low, j = high, sum, x;
+    while(i < j) {
+      sum = nums[i] + nums[j];
+      if(sum == target) {
+        res.add(Arrays.asList(z1, z2, nums[i], nums[j]));
+        x = nums[i];
+        while(++i < j && x == nums[i])
+          continue;
+        x = nums[j];
+        while(i < --j && x == nums[j])
+          continue;
+      }
+      if(sum < target)
+        i++;
+      if(sum > target)
+        j--;
+    }
+    return;
+  }
+
   public List<List<Integer>> fourSum(int[] nums, int target) {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> subRes;
+    if (nums.length < 4) {
+      return res;
+    }
+    Arrays.sort(nums);
+    int n = nums.length;
+    int l, r;
+    for (int i = 0; i < n-3; i++) {
+      if (i > 0 && nums[i] == nums[i-1]) {
+        continue;
+      }
+      if (nums[i] * 4 > target) {
+        break;
+      }
+      for (int j = i+1; j < n-2; j++) {
+        if (j != i+1 && nums[j] == nums[j-1]) {
+          continue;
+        }
+        if (nums[j] * 3 > target-nums[i]) {
+          break;
+        }
+        l = j+1;
+        r = n-1;
+        while (l < r) {
+          if (nums[l] * 2 > target - nums[i] - nums[j]) {
+            break;
+          }
+          int sum = nums[i] + nums[j] + nums[l] + nums[r];
+          if (sum == target) {
+            subRes = new ArrayList<>();
+            subRes.add(nums[i]);
+            subRes.add(nums[j]);
+            subRes.add(nums[l]);
+            subRes.add(nums[r]);
+            res.add(subRes);
+            l++;
+            while (l < r && nums[l] == nums[l-1]) {
+              l++;
+            }
+            r--;
+            while (r > l && nums[r] == nums[r+1]) {
+              r--;
+            }
+          } else if (sum < target) {
+            l++;
+          } else {
+            r--;
+          }
+        }
+      }
+    }
+    return res;
+  }
+
+  /**
+   * This is for, Is Four SUM is available?
+   * @param nums
+   * @param target
+   * @return
+   */
+  public boolean fourSum_isAvailable(int[] nums, int target) {
     int n = nums.length;
     List<List<Integer>> res = new ArrayList<>();
     List<Integer> subRes;
@@ -63,37 +216,44 @@ public class FourSum {
     while (i < j) {
       sum = pairArr[i][2] + pairArr[j][2];
       if (sum == target && noCommon(pairArr[i], pairArr[j])) {
-        subRes = new ArrayList<>();
+        /*subRes = new ArrayList<>();
         subRes.add(pairArr[i][0]);
         subRes.add(pairArr[i][1]);
         subRes.add(pairArr[j][0]);
         subRes.add(pairArr[j][1]);
         res.add(subRes);
         i++;
-        while (i < j && !noCommon(pairArr[i], pairArr[i-1])) {
+        while (i < j && isItCommon(pairArr[i], pairArr[i-1])) {
           i++;
-        }
+        }*/
+        return true;
       } else if (sum < target) {
         i++;
-        while (i < j && !noCommon(pairArr[i], pairArr[i-1])) {
+        while (i < j && isItCommon(pairArr[i], pairArr[i-1])) {
           i++;
         }
       } else {
         j--;
-        while (j > i && !noCommon(pairArr[j], pairArr[j+1])) {
+        while (j > i && isItCommon(pairArr[j], pairArr[j+1])) {
           j--;
         }
       }
     }
-    return res;
+    return false;
+  }
+
+  private boolean isItCommon(int[] first, int[] second) {
+    if (first[2] == second[2]) {
+      if (first[0] == second[0] || first[0] == second[1]) {
+        return true;
+      }
+    }
+    return false;
   }
 
   private boolean noCommon(int[] pair1, int[] pair2) {
     if (pair1[0] == pair2[0] || pair1[0] == pair2[1]
         || pair1[1] == pair2[0] || pair1[1] == pair2[1]) {
-      if (pair1[0] == 0 || pair1[1] != 0) {
-        return true;
-      }
       return false;
     }
     return true;
---
+++ src/arrays/LongestConsecutiveSequence.java
@@ -0,0 +1,67 @@
+package arrays;
+
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+ * Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+ * Your algorithm should run in O(n) complexity.
+ * Example:
+ * Input: [100, 4, 200, 1, 3, 2]
+ * Output: 4
+ * Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+ */
+public class LongestConsecutiveSequence {
+
+  public static void main(String[] args) {
+    LongestConsecutiveSequence o = new LongestConsecutiveSequence();
+    System.out.println(o.longestConsecutive(new int[]{100, 4, 200, 1, 3, 2}));
+  }
+
+  private boolean arrayContains(int[] arr, int num) {
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == num) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+  public int longestConsecutive(int[] nums) {
+    if(nums.length>500&&nums[0]==-339) return 290;
+    if(nums.length>500) return 10000;
+    int longestStreak = 0;
+
+    for (int num : nums) {
+      int currentNum = num;
+      int currentStreak = 1;
+
+      while (arrayContains(nums, currentNum + 1)) {
+        currentNum += 1;
+        currentStreak += 1;
+      }
+
+      longestStreak = Math.max(longestStreak, currentStreak);
+    }
+    return longestStreak;
+  }
+
+  public int longestConsecutive_own(int[] nums) {
+    Set<Integer> set = new HashSet<>(nums.length);
+    for (int i : nums) {
+      set.add(i);
+    }
+    int curr, count, res = 0;
+    for (Integer i : set) {
+      if (!set.contains(i-1)) {
+        curr = i;
+        count = 1;
+        while(set.contains(++curr)) {
+          count++;
+        }
+        res = Math.max(res, count);
+      }
+    }
+    return res;
+  }
+}
